d5aadb7c6b8080e5894362b85cbe8a0ab67c1b5d
Jamie Rushford <jar.rushford@gmail.com>
2019-02-24 12:54:13 -0700

    Seed data in dev env

---
+++ utils/mockData.js
@@ -0,0 +1,56 @@
+const users = [
+  {
+    email: 'madwallaceband@gmail.com',
+    password: 'Davidshaiku'
+  }
+]
+
+const tour_dates = [
+  {
+    day_of_week: 'Wednesday',
+    date: '03/13',
+    city: 'Denver, CO',
+    venue: 'Lost Lake',
+    ticket_link: 'https://www.lost-lake.com/event/1822119-cbdb-denver/',
+    venue_link: 'https://www.lost-lake.com/'
+  },
+  {
+    day_of_week: 'Friday',
+    date: '03/27',
+    city: 'Denver, CO',
+    venue: 'Summit',
+    ticket_link: 'http://www.summitdenver.com/',
+    venue_link: 'http://www.summitdenver.com/'
+  }
+]
+
+const news = [
+  {
+    title: 'New Album!',
+    body: 'Give it a listen',
+    link: 'https://madwallace.bandcamp.com/releases',
+    image_url: 'http://www.madwallace.com/images/hookline-album-art.jpg'
+  }
+]
+
+const photos = [
+  {
+    link: 'http://www.madwallace.com/images/hookline-album-art.jpg',
+    description: 'New album art'
+  }
+]
+
+const videos = [
+  {
+    link: 'https://www.youtube.com/watch?v=dAPQgkgLNRw',
+    title: 'Limestone @ Herman\'s Hideaway'
+  }
+]
+
+module.exports = {
+  users,
+  tour_dates,
+  news,
+  photos,
+  videos
+}
